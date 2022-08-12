1fc5b5fdb2cf79789583c1f9dfcdd97308c8cf29
Ronnie Hicks <hicks.ronnie@gmail.com>
2022-08-12 18:35:01 +0000

    chore: Cleaning up example file

--- examples/main.tf
+++ examples/main.tf
@@ -13,14 +13,11 @@ variable "P1_CLIENT_SECRET" {
   sensitive   = true
 }
 provider "pingone" {
-  client_id     = "3c789e2c-4900-4772-8d79-1148ef3baab1"
+  client_id     = "client_id"
   client_secret = var.P1_CLIENT_SECRET
 }
 
 locals {
-  environment_id = "5c3a5b10-ce82-4f4b-80de-6fc187022f35"
+  environment_id = "env_id"
 }
 
-locals {
-  environment_id = "envId"
-}
